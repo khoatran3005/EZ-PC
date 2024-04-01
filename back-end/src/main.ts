4fa2b15221f52fd3a1d7f8c05691b24d93fb3dde
Khoa Tran <khoatran3005@gmail.com>
2024-04-01 00:27:01 -0400

    register

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -5,6 +5,16 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  app.enableCors({
+    origin: 'http://localhost:3001', // or true to allow all origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // specify the allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // specify the allowed headers
+    credentials: true, // allow sending credentials like cookies or authorization headers
+  });
+
   await app.listen(configService.get<string>('PORT'));
+
+
 }
 bootstrap();
